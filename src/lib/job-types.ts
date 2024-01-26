9f54ec9daa9855979d6509ad960fecbbd0dbcbfd
Ridha <ridhauwu@gmail.com>
2024-01-26 17:33:33 +0700

    4: Server action in server component

---
+++ src/lib/job-types.ts
@@ -0,0 +1,12 @@
+const jobTypes = [
+  "Full-time",
+  "Part-time",
+  "Contract",
+  "Temporary",
+  "Internship",
+  "Volunteer",
+];
+
+const locationTypes = ["Remote", "Hybrid", "On-site"];
+
+export { jobTypes, locationTypes };
